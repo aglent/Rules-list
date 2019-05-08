8d6164953589ca0e6fd79e7145c00d58cd09af74
Jaleo <jaleo.fu@gmail.com>
2019-05-08 18:52:14 +0800

    Update Wb_AD.js

--- Surge3/Script/Wb_AD.js
+++ Surge3/Script/Wb_AD.js
@@ -7,7 +7,7 @@
 /*
 [Script]
 # In-app advertising
-http-response ^https?:\/\/api\.weibo\.cn\/2(\/groups\/timeline|\/statuses\/unread|\/statuses\/extend|\/comments\/build_comments|\/photo\/recommend_list|\/stories\/video_stream|\/statuses\/positives\/get|\/stories\/home_list|\/profile\/statuses|\/statuses\/friends\/timeline) script-path=https://raw.githubusercontent.com/yichahucha/surge/master/wb_ad.js
+http-response ^https?:\/\/api\.weibo\.cn\/2(\/groups\/timeline|\/statuses\/unread|\/statuses\/extend|\/comments\/build_comments|\/photo\/recommend_list|\/stories\/video_stream|\/statuses\/positives\/get|\/stories\/home_list|\/profile\/statuses|\/statuses\/friends\/timeline) script-path=https://raw.githubusercontent.com/aglent/Rules-list/master/Surge3/Script/Wb_AD.js
 # Launch advertising
 http-response ^https?:\/\/(sdk|wb)app\.uve\.weibo\.com(\/interface\/sdk\/sdkad.php|\/wbapplua\/wbpullad.lua) script-path=https://raw.githubusercontent.com/yichahucha/surge/master/wb_launch.js
 [MITM]
