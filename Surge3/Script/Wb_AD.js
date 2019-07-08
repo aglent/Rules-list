098d85d6ecabfd3ab8b65f841766e0485e13dc52
Jaleo <jaleo.fu@gmail.com>
2019-07-08 15:56:56 +0800

    Update Wb_AD.js

--- Surge3/Script/Wb_AD.js
+++ Surge3/Script/Wb_AD.js
@@ -1,19 +1,3 @@
-/*
- * @repo: https://github.com/yichahucha/surge
- * @script: https://raw.githubusercontent.com/yichahucha/surge/master/wb_ad.js
- * @doc: https://raw.githubusercontent.com/yichahucha/surge/master/README.md
- * @regular: ^https?:\/\/api\.weibo\.cn\/2(\/groups\/timeline|\/statuses\/unread|\/statuses\/extend|\/comments\/build_comments|\/photo\/recommend_list|\/stories\/video_stream|\/statuses\/positives\/get|\/stories\/home_list|\/profile\/statuses|\/statuses\/friends\/timeline)
- */
-/*
-[Script]
-# In-app advertising
-http-response ^https?:\/\/api\.weibo\.cn\/2(\/groups\/timeline|\/statuses\/unread|\/statuses\/extend|\/comments\/build_comments|\/photo\/recommend_list|\/stories\/video_stream|\/statuses\/positives\/get|\/stories\/home_list|\/profile\/statuses|\/statuses\/friends\/timeline) script-path=https://raw.githubusercontent.com/aglent/Rules-list/master/Surge3/Script/Wb_AD.js
-# Launch advertising
-http-response ^https?:\/\/(sdk|wb)app\.uve\.weibo\.com(\/interface\/sdk\/sdkad.php|\/wbapplua\/wbpullad.lua) script-path=https://raw.githubusercontent.com/aglent/Rules-list/master/Surge3/Script/Wb_Launch.js
-[MITM]
-hostname = api.weibo.cn, *.uve.weibo.com
-*/
-
 const path1 = "/groups/timeline";
 const path2 = "/statuses/unread";
 const path3 = "/statuses/extend";
@@ -24,58 +8,46 @@ const path7 = "/statuses/positives/get";
 const path8 = "/stories/home_list";
 const path9 = "/profile/statuses";
 const path10 = "/statuses/friends/timeline";
+const path11 = "/service/picfeed";
 
-var result = body;
-function is_likerecommend(title) {
-    if (title && title.type && title.type == "likerecommend") {
-        return true;
-    } else {
-        return false;
-    }
-}
-
+const url = $request.url;
+var body = $response.body;
 function filter_timeline() {
     let obj = JSON.parse(body);
     let statuses = obj.statuses;
     if (statuses && statuses.length > 0) {
-        let ad = obj.ad;
-        if (ad && ad.length > 0) {
-            for (let i = 0; i < ad.length; i++) {
-                const element = ad[i];
-                let ad_id = element.id;
-                for (let j = 0; j < statuses.length; j++) {
-                    const element = statuses[j];
-                    let statuses_id = element.id;
-                    if (statuses_id == parseInt(ad_id)) {
-                        statuses.splice(j, 1);
-                        break;
-                    }
-                }
-            }
-        }
-
         let i = statuses.length;
         while (i--) {
             let element = statuses[i];
-            if (is_likerecommend(element.title)) {
-                statuses.splice(i, 1);
-            }
-            if (element.pic_bg_new) {
-                delete element.pic_bg_new;
-                delete element.pic_bg_type;
-            }
+            if (is_timeline_likerecommend(element.title)) statuses.splice(i, 1);
+            if(is_timeline_ad(element.promotion)) statuses.splice(i, 1);
         }
+        if (obj.advertises) obj.advertises = [];
+        if (obj.ad) obj.ad = [];
+        if (obj.num) obj.num = obj.original_num;
+    }
+    if (obj.trends) obj.trends = [];
+    body = JSON.stringify(obj);
+}
 
-        if (obj.num) {
-            obj.num = obj.statuses.length + ad.length;
-            obj.original_num = obj.statuses.length;
+function is_timeline_ad(promotion) {
+    return (promotion && promotion.type && promotion.type == "ad") ? true : false;
+}
+
+function is_timeline_likerecommend(title) {
+    return (title && title.type && title.type == "likerecommend") ? true : false;
+}
+
+function filter_comments(datas) {
+    if (datas && datas.length > 0) {
+        let i = datas.length;
+        while (i--) {
+            const element = datas[i];
+            let type = element.type;
+            if (type == 5 || type == 1 || type == 6) datas.splice(i, 1);
         }
     }
-
-    if (obj.trends) {
-        obj.trends = [];
-    }
-    result = JSON.stringify(obj);
+    return datas;
 }
 
 if (url.indexOf(path1) != -1) {
@@ -86,36 +58,31 @@ if (url.indexOf(path2) != -1) {
     filter_timeline();
 }
 
-if (url.indexOf(path10) != -1) {
-    filter_timeline();
-}
-
 if (url.indexOf(path3) != -1) {
     let obj = JSON.parse(body);
-    delete obj.trend;
-    result = JSON.stringify(obj);
+    if (obj.trend) delete obj.trend;
+    body = JSON.stringify(obj);
 }
 
 if (url.indexOf(path4) != -1) {
     let obj = JSON.parse(body);
-    let datas = obj.datas;
-    if (datas && datas.length > 0) {
-        let i = datas.length;
-        while (i--) {
-            const element = datas[i];
-            let type = element.type;
-            if (type == 5 || type == 1 || type == 6) {
-                datas.splice(i, 1);
-            }
+    obj.recommend_max_id = 0;
+    if (obj.status) {
+        if (obj.top_hot_structs) {
+            obj.max_id = obj.top_hot_structs.call_back_struct.max_id;
+            delete obj.top_hot_structs;
         }
+        if (obj.datas) obj.datas = filter_comments(obj.datas);
+    } else {
+        obj.datas = [];
     }
-    result = JSON.stringify(obj);
+    body = JSON.stringify(obj);
 }
 
 if (url.indexOf(path5) != -1) {
     let obj = JSON.parse(body);
     obj.data = {};
-    result = JSON.stringify(obj);
+    body = JSON.stringify(obj);
 }
 
 if (url.indexOf(path6) != -1) {
@@ -126,24 +93,22 @@ if (url.indexOf(path6) != -1) {
         while (i--) {
             const element = segments[i];
             let is_ad = element.is_ad;
-            if (typeof is_ad != "undefined" && is_ad == true) {
-                segments.splice(i, 1);
-            }
+            if (typeof is_ad != "undefined" && is_ad == true) segments.splice(i, 1);
         }
     }
-    result = JSON.stringify(obj);
+    body = JSON.stringify(obj);
 }
 
 if (url.indexOf(path7) != -1) {
     let obj = JSON.parse(body);
     obj.datas = [];
-    result = JSON.stringify(obj);
+    body = JSON.stringify(obj);
 }
 
 if (url.indexOf(path8) != -1) {
     let obj = JSON.parse(body);
     obj.story_list = [];
-    result = JSON.stringify(obj);
+    body = JSON.stringify(obj);
 }
 
 if (url.indexOf(path9) != -1) {
@@ -154,65 +119,19 @@ if (url.indexOf(path9) != -1) {
         while (i--) {
             let element = cards[i];
             let card_group = element.card_group;
-            if (card_group && card_group.length > 0) {
-                cards.splice(i, 1);
-            }
+            if (card_group && card_group.length > 0) cards.splice(i, 1);
         }
     }
-    result = JSON.stringify(obj);
+    body = JSON.stringify(obj);
 }
-result;
-    }
-    json_body.statuses = new_statuses;
-    result = JSON.stringify(json_body);
-}
-if (url.indexOf(path1) != -1) {
+
+if (url.indexOf(path10) != -1) {
     filter_timeline();
 }
-if (url.indexOf(path2) != -1) {
-    filter_timeline();
+
+if (url.indexOf(path11) != -1) {
+    let obj = JSON.parse(body);
+    obj.data = [];
+    body = JSON.stringify(obj);
 }
-if (url.indexOf(path3) != -1) { 
-    var json_body = JSON.parse(body);
-    delete json_body.trend
-    result = JSON.stringify(json_body);
-}
-if (url.indexOf(path4) != -1) { 
-    var json_body = JSON.parse(body);
-    var datas = json_body.datas;
-    var new_datas = []
-    for (let j = 0; j < datas.length; j++) {
-        const element = datas[j];
-        let type = element.type;
-        if (type!=5 && type!=1 && type!=6) {
-            new_datas.push(element);
-        }
-    }
-    json_body.datas = new_datas;
-    result = JSON.stringify(json_body);
-}
-if (url.indexOf(path5) != -1) { 
-    var json_body = JSON.parse(body);
-    json_body.data = {};
-    result = JSON.stringify(json_body);
-}
-if (url.indexOf(path6) != -1) { 
-    var json_body = JSON.parse(body);
-    var segments = json_body.segments;
-    var new_segments = [];
-    for (let j = 0; j < segments.length; j++) {
-        const element = segments[j];
-        let is_ad = element.is_ad;
-        if (typeof(is_ad) == "undefined" || is_ad == false) {
-            new_segments.push(element);
-        }
-    }
-    json_body.segments = new_segments;
-    result = JSON.stringify(json_body);
-}
-if (url.indexOf(path7) != -1) {
-    var json_body = JSON.parse(body);
-    json_body.datas = [];
-    result = JSON.stringify(json_body);
-}
-result;
+$done({body});
