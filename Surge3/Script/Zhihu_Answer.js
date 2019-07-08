9ec8af31e2c9f7b57becaf60f77c54d2f941a16b
Jaleo <jaleo.fu@gmail.com>
2019-07-08 16:14:56 +0800

    Update Zhihu_Answer.js

--- Surge3/Script/Zhihu_Answer.js
+++ Surge3/Script/Zhihu_Answer.js
@@ -1,15 +1,3 @@
-// 知乎去广告 （作者：onewayticket255）
-// https://github.com/onewayticket255/Surge-Script/blob/master/surge%20zhihu%20answer.js
-
-// [Script]
-// http-response https://api.zhihu.com/topstory/follow requires-body=true,script-path=https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20feed.js
-// http-response https://api.zhihu.com/topstory/recommend requires-body=true,script-path=https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20recommend.js
-// http-response https://api.zhihu.com/.*/questions requires-body=true,script-path=https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20answer.js
-// http-response https://api.zhihu.com/market/header requires-body=true,script-path=https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20market.js
-
-// [MITM]
-// hostname = api.zhihu.com
-
 let body = $response.body
 body=JSON.parse(body)
 delete body['ad_info']
