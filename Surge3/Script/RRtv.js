348d2dfe75258e4bef9545fd555aa8fe7a8d8882
Jaleo <jaleo.fu@gmail.com>
2019-07-08 16:32:59 +0800

    Update RRtv.js

--- Surge3/Script/RRtv.js
+++ Surge3/Script/RRtv.js
@@ -1,41 +1,14 @@
-// https://github.com/ydzydzydz/Rules/blob/master/resources/js/RRtv.js
+var result = body;
 
-var result = body
-
-let path1 = '/user/profile';
-let path2 = '/v3plus/user/detail';
-let path3 = '/ad/getAll';
-
-if (url.indexOf(path1) != -1) {
-    var jsbody = JSON.parse(body);
-    jsbody.data.user.medalList = JSON.parse('[{"name":"原画","endTime":"2022-12-22 22:22:22","imgUrl":"http://img.rr.tv/screenshot/20171108/o_1510128764030.png","id":1}]');
-    jsbody.data.user.privilegeList = JSON.parse('[{"effectObject":"video","action":"play","func":"originalPainting","description":"解锁原画","icon":"jiesuoyuanhua","endTime":1671718942000},{"effectObject":"coin","action":"sign","func":"5","description":"签到额外银币+5","icon":"qiandaoyinbi","endTime":1671718942000},{"effectObject":"growth","action":"play","func":"0.4","description":"经验值加成+40%","icon":"jingyanzhijiacheng","endTime":1671718942000},{"effectObject":"video","action":"play","func":"noLimit","description":"看剧无限制","icon":"kanjuwuxianzhi","endTime":1671718942000},{"effectObject":"video","action":"play","func":"noAd","description":"看剧无广告","icon":"kanjuwuguanggao","endTime":1671718942000}]');
-    jsbody.data.user.createTime = 1671718942000;
-    jsbody.data.user.newUser = true;
-    jsbody.data.user.level = 30;
-    jsbody.data.user.nickName = 'Choler';
-    jsbody.data.user.achievementCount = 9999;
-    result = JSON.stringify(jsbody);
+if (url.indexOf('/ad/getAll') != -1) {
+    var body = JSON.parse(body);
+    body.data.adList = [];
+    result = JSON.stringify(body);
+} else if (url.indexOf('/user/profile') != -1) {
+    var body = JSON.parse(body);
+    body.data.user.medalList = JSON.parse('[{"name":"看剧必备","endTime":"4101494400000","imgUrl":"http://img.rr.tv/screenshot/20171108/o_1510128764030.png","id":1}]');
+    body.data.user.privilegeList = JSON.parse('[{"effectObject":"video","action":"play","func":"originalPainting","description":"解锁原画","icon":"jiesuoyuanhua","endTime":4101494400000},{"effectObject":"coin","action":"sign","func":"5","description":"签到额外银币+5","icon":"qiandaoyinbi","endTime":4101494400000},{"effectObject":"growth","action":"play","func":"0.4","description":"经验值加成+40%","icon":"jingyanzhijiacheng","endTime":4101494400000},{"effectObject":"video","action":"play","func":"noLimit","description":"看剧无限制","icon":"kanjuwuxianzhi","endTime":4101494400000},{"effectObject":"video","action":"play","func":"noAd","description":"看剧无广告","icon":"kanjuwuguanggao","endTime":4101494400000}]');
+    body.data.user.level = 30;
+    result = JSON.stringify(body);
 }
-
-if (url.indexOf(path2) != -1) {
-    var jsbody = JSON.parse(body);
-    jsbody.data.user.medalList = JSON.parse('[{"name":"yel","endTime":"2022-12-22 22:22:22","imgUrl":"http://img.rr.tv/screenshot/20171108/o_1510128764030.png","id":1}]');
-    result = JSON.stringify(jsbody);
-}
-
-if (url.indexOf(path3) != -1) {
-    var jsbody = JSON.parse(body);
-    jsbody.data.adList = [];
-    result = JSON.stringify(jsbody);
-}
-
 result;
-
-
-
-/*********************************************************
- *来源：https://github.com/Choler/Surge/blob/master/RRtv.js
- *http-response ^https?://api\.rr\.tv/ script-path= https://github.com/Choler/Surge/raw/master/RRtv.js
- *hostname = api.rr.tv
- ********************************************************/
