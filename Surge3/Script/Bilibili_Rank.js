29298ae41ffbd6f25fbdf31504f65e262f380cbd
Jaleo <jaleo.fu@gmail.com>
2019-07-08 16:28:30 +0800

    Update Bilibili_Rank.js

--- Surge3/Script/Bilibili_Rank.js
+++ Surge3/Script/Bilibili_Rank.js
@@ -1,17 +1,3 @@
-/* https://github.com/onewayticket255/Surge-Script/blob/master/surge%20bilibili%20rank.js
-
-[Script]
-# 哔哩哔哩去广告 （作者：onewayticket255）
-http-response https://app.bilibili.com/x/resource/show/tab requires-body=true,script-path=https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20bilibili%20tab.js
-http-response https://app.bilibili.com/x/v2/feed requires-body=true,script-path=https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20bilibili%20feed.js
-http-response https://app.bilibili.com/x/v2/account/mine requires-body=true,script-path=https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20bilibili%20account.js
-http-response https://app.bilibili.com/x/v2/view\?access_key requires-body=true,script-path=https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20bilibili%20view%20relate.js
-http-response https://app.bilibili.com/x/v2/rank requires-body=true,script-path=https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20bilibili%20rank.js
-
-[MITM]
-hostname = app.bilibili.com, api.bilibili.com
-*/
-
 let body = $response.body
 body=JSON.parse(body)
 body['data'].forEach((element, index)=> {
