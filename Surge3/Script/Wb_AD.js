510f911ae42bae914be2cb7c5ffdfa8f2a2614bf
Jaleo <jaleo.fu@gmail.com>
2019-10-21 14:45:09 +0800

    Update Wb_AD.js

--- Surge3/Script/Wb_AD.js
+++ Surge3/Script/Wb_AD.js
@@ -159,21 +159,10 @@ function filter_timeline_cards(cards) {
                         if (is_timeline_ad(card_group_item.mblog)) card_group.splice(i, 1);
                     } else if (card_type && card_type == 118) {
                         card_group.splice(i, 1);
-                    } else if (card_type && card_type == 4) {
-                        if (card_group_item.promotion) card_group.splice(i, 1);
                     } else if (card_type && card_type == 42) {
                         if (card_group_item.desc == '\u53ef\u80fd\u611f\u5174\u8da3\u7684\u4eba') {
                             cards.splice(j, 1);
                             break;
-                        }
-                    } else if (card_type && card_type == 17) {
-                        let group = card_group_item.group;
-                        if (group && group.length > 0) {
-                            let k = group.length;
-                            while (k--) {
-                                let group_item = group[k];
-                                if (group_item.promotion) group.splice(k, 1);
-                            }
                         }
                     }
                 }
